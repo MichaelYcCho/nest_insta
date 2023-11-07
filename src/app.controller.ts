b5d1026ea1db1dc21b8040d693db4c512ff1d603
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-11-07 23:04:10 +0900

    feat: Controller Interface 활용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'John',
+      title: 'My first post',
+      content: 'This is my first post',
+      likeCount: 0,
+      commentCount: 0,
+    };
   }
 }
